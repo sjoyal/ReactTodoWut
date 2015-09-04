b327f1608ddb3a61fa176e5b2fc6f0094bf8a3b3
Sean Joyal <sean.joyal@gmail.com>
2015-09-04 16:33:55 -0400

    initial js scaffolding completed

--- app/js/todo.js
+++ app/js/todo.js
@@ -1,17 +1,32 @@
 (function(){
 
+  // constructor for inputting a new task
   function taskAdded(task){
-    
-  }
+    this.task = task;
+    this.completed = false;
+  };
 
   // this is the ViewModel
   function ToDoViewModel(){
-    // do stuff in here:
-    // var self = this;
-    // this.tasks = [ ]
-    // this.addTask = function(){};
-    // this.removeTask = function(){};
-    // this.completeTask = function(){};
+    var self = this;
+    this.tasks = [
+      new taskAdded('Drink the beers'),
+      new taskAdded('Read the books')
+    ];
+    this.addTask = function(task){
+      self.tasks.push(new taskAdded(task));
+    };
+    // research this .remove() method, is this from knockout and reads the index #?
+    this.removeTask = function(task){
+      self.tasks.remove(task);
+    };
+    // FIXME: This probably isn't right, research how to feed it the approp. index
+    this.completeTask = function(index){
+      self.tasks[index].completed = true;
+    };
   };
+
+  // apply bindings to the browser
+  ko.applyBindings(new ToDoViewModel());
 
 })();
