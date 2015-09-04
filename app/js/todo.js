09161b962f05071547039a889dceb83e36a4e5d4
Sean Joyal <sean.joyal@gmail.com>
2015-09-04 15:53:25 -0400

    added new task constructor function and main viewmodel function scaffolding

--- app/js/todo.js
+++ app/js/todo.js
@@ -1,5 +1,17 @@
 (function(){
 
-  // put a todo list app here
+  function taskAdded(task){
+    
+  }
+
+  // this is the ViewModel
+  function ToDoViewModel(){
+    // do stuff in here:
+    // var self = this;
+    // this.tasks = [ ]
+    // this.addTask = function(){};
+    // this.removeTask = function(){};
+    // this.completeTask = function(){};
+  };
 
 })();
