93fa8e4819cf7bea301bee6a508aabc544a9e4f2
Sean Joyal <sean.joyal@gmail.com>
2015-09-05 15:21:48 -0400

    added data-bind attributes to html with foreach binding to display each list item

--- app/js/todo.js
+++ app/js/todo.js
@@ -9,16 +9,18 @@
   // this is the ViewModel
   function ToDoViewModel(){
     var self = this;
-    this.tasks = [
+    this.tasks = ko.observableArray([
       new taskAdded('Drink the beers'),
-      new taskAdded('Read the books')
-    ];
+      new taskAdded('Read the books'),
+      new taskAdded('Watch the shows')
+    ]);
     this.addTask = function(task){
       self.tasks.push(new taskAdded(task));
     };
     // research this .remove() method, is this from knockout and reads the index #?
     this.removeTask = function(task){
       self.tasks.remove(task);
+      // self.tasks.splice(index, 1);
     };
     // FIXME: This probably isn't right, research how to feed it the approp. index
     this.completeTask = function(index){
