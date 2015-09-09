3cda0ddf30de5f6e00b2b8ddcce644a7d29b3106
Sean Joyal <sean.joyal@gmail.com>
2015-09-09 17:14:01 -0400

    adding tasks with form submission, resetting input field afterward

--- app/js/todo.js
+++ app/js/todo.js
@@ -1,30 +1,27 @@
 (function(){
 
+  // constructor for inputting a new task
+  function taskAdded(todo){
+    this.todo = todo;
+    this.completed = ko.observable(false);
+  };
+
   // this is the ViewModel
   function ToDoViewModel(){
     var self = this;
-    // constructor for inputting a new task
-    function taskAdded(todo){
-      this.todo = todo;
-      this.completed = ko.observable(false);
-    };
+    this.thisToDo = ko.observable('');
     this.tasks = ko.observableArray([
       new taskAdded('Drink the beers'),
       new taskAdded('Read the books'),
       new taskAdded('Watch the shows')
     ]);
-    this.addTask = function(task){
-      self.tasks.push(new taskAdded(task));
+    this.addTask = function(){
+      self.tasks.push(new taskAdded(self.thisToDo()));
+      self.thisToDo('');
     };
-    // research this .remove() method, is this from knockout and reads the index #?
     this.removeTask = function($index){
       self.tasks.remove($index);
-      // self.tasks.splice(index, 1);
     };
-    // FIXME: This probably isn't right, research how to feed it the approp. index
-    // this.completeTask = function($index){
-    //   self.tasks[$index].completed = true;
-    // };
   };
 
   // apply bindings to the browser
