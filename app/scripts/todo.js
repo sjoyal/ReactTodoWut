680113327cba86f72e5f3430ceea57bc56bf98d1
Sean Joyal <sean.joyal@gmail.com>
2015-11-06 00:41:23 -0500

    fiddling with a computed to list a banner saying no tasks left when all are marked as completed

--- app/scripts/todo.js
+++ app/scripts/todo.js
@@ -4,12 +4,13 @@
   // constructor for inputting a new task
   function taskAdded(todo){
     this.todo = todo;
-    this.completed = ko.observable(false);
+    this.completed = ko.observable(true);
   };
 
   // this is the ViewModel
   function ToDoViewModel(){
     var self = this;
+
     this.thisToDo = ko.observable('');
     this.tasks = ko.observableArray([
       new taskAdded('Drink the beers'),
@@ -20,9 +21,18 @@
       self.tasks.push(new taskAdded(self.thisToDo()));
       self.thisToDo('');
     };
-    this.removeTask = function($index){
-      self.tasks.remove($index);
+    this.removeTask = function(task){
+      self.tasks.remove(task);
     };
+
+    this.tasksRemaining = ko.pureComputed(function(){
+      var anyTasks = _.find(this.tasks, function(task){
+        task.completed === true;
+      });
+      console.log(anyTasks);
+      return anyTasks;
+    }, this);
+    console.log(this.tasksRemaining);
   };
 
   // apply bindings to the browser
